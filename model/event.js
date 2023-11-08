ada6988c3626eac48011cd0676e508ae5b5a3f8c
Faizan Manzoor Mir <141254735+FaizanMir01@users.noreply.github.com>
2023-11-08 15:48:06 +0530

    Update event.js

--- model/event.js
+++ model/event.js
@@ -1,27 +1,35 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    event_title:{
-        type:String
-    },
-    event_date:{
-        type:Date
-    },
-    event_location:{
-        type:String
-    },
-    event_description:{
-        type:String
-    },
-    event_organizer:{
-        type:String
-    },
-    volunteers_needed:{
-        type:Number
-    }
+const eventSchema = new mongoose.Schema({
+  event_title: {
+    type: String,
+  },
+  event_type:{
+    type:String
+  },
+  event_email:{
+    type: String
+  },
+  event_date: {
+    type: Date,
+  },
+  event_location: {
+    type: String,
+  },
+  event_description: {
+    type: String,
+  },
+  event_organizer: {
+    type: String,
+  },
+  volunteers_needed: {
+    type: Number,
+  },
+  Group_Link:{
+    type: String
+  }
+});
 
-})
-const eventData = mongoose.model('eventData', userSchema)
+const eventData = mongoose.model('eventData', eventSchema);
 
-
-module.exports = eventData
+module.exports = eventData;
